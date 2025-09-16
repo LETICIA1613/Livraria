6e421cb0cca9890df8561e833bafc5ecf1a1a3ce
LETICIA1613 <leticiastefanidombosco@gmail.com.br>
2025-09-16 12:53:59 -0300

    genero

--- Livraria/Telaadicionarlivro.Designer.cs
+++ Livraria/Telaadicionarlivro.Designer.cs
@@ -29,80 +29,74 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Telaadicionarlivro));
-            this.btnadd = new System.Windows.Forms.Button();
+            this.Btnadd = new System.Windows.Forms.Button();
             this.Txtnamebook = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtAutor = new System.Windows.Forms.TextBox();
+            this.TxtEditora = new System.Windows.Forms.TextBox();
+            this.Picturebook = new System.Windows.Forms.PictureBox();
+            this.TxtPreco = new System.Windows.Forms.TextBox();
             this.Btnimage = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtGenero = new System.Windows.Forms.TextBox();
+            this.ListGenero = new System.Windows.Forms.ListBox();
+            this.ComboGenero = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picturebook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnadd
+            // Btnadd
             // 
-            this.btnadd.BackColor = System.Drawing.Color.Pink;
-            this.btnadd.Location = new System.Drawing.Point(363, 365);
-            this.btnadd.Name = "btnadd";
-            this.btnadd.Size = new System.Drawing.Size(75, 23);
-            this.btnadd.TabIndex = 1;
-            this.btnadd.Text = "Adicionar";
-            this.btnadd.UseVisualStyleBackColor = false;
+            this.Btnadd.BackColor = System.Drawing.Color.Pink;
+            this.Btnadd.Location = new System.Drawing.Point(402, 357);
+            this.Btnadd.Name = "Btnadd";
+            this.Btnadd.Size = new System.Drawing.Size(75, 23);
+            this.Btnadd.TabIndex = 1;
+            this.Btnadd.Text = "Adicionar";
+            this.Btnadd.UseVisualStyleBackColor = false;
+            this.Btnadd.Click += new System.EventHandler(this.Btnadd_Click);
             // 
             // Txtnamebook
             // 
-            this.Txtnamebook.Location = new System.Drawing.Point(175, 235);
+            this.Txtnamebook.Location = new System.Drawing.Point(175, 256);
             this.Txtnamebook.Name = "Txtnamebook";
-            this.Txtnamebook.Size = new System.Drawing.Size(217, 20);
+            this.Txtnamebook.Size = new System.Drawing.Size(185, 20);
             this.Txtnamebook.TabIndex = 2;
             // 
-            // textBox2
+            // TxtAutor
             // 
-            this.textBox2.Location = new System.Drawing.Point(175, 261);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(217, 20);
-            this.textBox2.TabIndex = 3;
+            this.TxtAutor.Location = new System.Drawing.Point(175, 282);
+            this.TxtAutor.Name = "TxtAutor";
+            this.TxtAutor.Size = new System.Drawing.Size(185, 20);
+            this.TxtAutor.TabIndex = 3;
             // 
-            // textBox3
+            // TxtEditora
             // 
-            this.textBox3.Location = new System.Drawing.Point(175, 287);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(217, 20);
-            this.textBox3.TabIndex = 4;
+            this.TxtEditora.Location = new System.Drawing.Point(175, 308);
+            this.TxtEditora.Name = "TxtEditora";
+            this.TxtEditora.Size = new System.Drawing.Size(185, 20);
+            this.TxtEditora.TabIndex = 4;
             // 
-            // pictureBox2
+            // Picturebook
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(175, 101);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 118);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
+            this.Picturebook.Location = new System.Drawing.Point(175, 102);
+            this.Picturebook.Name = "Picturebook";
+            this.Picturebook.Size = new System.Drawing.Size(100, 129);
+            this.Picturebook.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Picturebook.TabIndex = 5;
+            this.Picturebook.TabStop = false;
             // 
-            // pictureBox3
+            // TxtPreco
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(-5, -11);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(819, 480);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 6;
-            this.pictureBox3.TabStop = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(175, 313);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(217, 20);
-            this.textBox1.TabIndex = 7;
+            this.TxtPreco.Location = new System.Drawing.Point(175, 334);
+            this.TxtPreco.Name = "TxtPreco";
+            this.TxtPreco.Size = new System.Drawing.Size(185, 20);
+            this.TxtPreco.TabIndex = 7;
             // 
             // Btnimage
             // 
             this.Btnimage.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.Btnimage.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Btnimage.Location = new System.Drawing.Point(281, 196);
+            this.Btnimage.Location = new System.Drawing.Point(281, 208);
             this.Btnimage.Name = "Btnimage";
             this.Btnimage.Size = new System.Drawing.Size(111, 23);
             this.Btnimage.TabIndex = 8;
@@ -110,36 +104,75 @@
             this.Btnimage.UseVisualStyleBackColor = false;
             this.Btnimage.Click += new System.EventHandler(this.Btnimage_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(807, 549);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtGenero
+            // 
+            this.TxtGenero.Location = new System.Drawing.Point(520, 320);
+            this.TxtGenero.Name = "TxtGenero";
+            this.TxtGenero.Size = new System.Drawing.Size(185, 20);
+            this.TxtGenero.TabIndex = 10;
+            // 
+            // ListGenero
+            // 
+            this.ListGenero.FormattingEnabled = true;
+            this.ListGenero.Location = new System.Drawing.Point(536, 179);
+            this.ListGenero.Name = "ListGenero";
+            this.ListGenero.Size = new System.Drawing.Size(185, 17);
+            this.ListGenero.TabIndex = 11;
+            // 
+            // ComboGenero
+            // 
+            this.ComboGenero.FormattingEnabled = true;
+            this.ComboGenero.Location = new System.Drawing.Point(175, 360);
+            this.ComboGenero.Name = "ComboGenero";
+            this.ComboGenero.Size = new System.Drawing.Size(185, 21);
+            this.ComboGenero.TabIndex = 12;
+            // 
             // Telaadicionarlivro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Btnimage);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnadd);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ComboGenero);
+            this.Controls.Add(this.ListGenero);
+            this.Controls.Add(this.TxtGenero);
+            this.Controls.Add(this.Btnadd);
+            this.Controls.Add(this.TxtPreco);
+            this.Controls.Add(this.TxtEditora);
+            this.Controls.Add(this.TxtAutor);
             this.Controls.Add(this.Txtnamebook);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.Btnimage);
+            this.Controls.Add(this.Picturebook);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Telaadicionarlivro";
             this.Text = "Telaadicionarlivro";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picturebook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button Btnadd;
         private System.Windows.Forms.TextBox Txtnamebook;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtAutor;
+        private System.Windows.Forms.TextBox TxtEditora;
+        private System.Windows.Forms.PictureBox Picturebook;
+        private System.Windows.Forms.TextBox TxtPreco;
         private System.Windows.Forms.Button Btnimage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TxtGenero;
+        private System.Windows.Forms.ListBox ListGenero;
+        private System.Windows.Forms.ComboBox ComboGenero;
     }
 }
--- Livraria/Telaadicionarlivro.cs
+++ Livraria/Telaadicionarlivro.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace Livraria
 {
     public partial class Telaadicionarlivro : Form
     {
+        
         public Telaadicionarlivro()
         {
             InitializeComponent();
         }
-
-
-
+        string sql = "INSERT INTO LivroGeneros (LivroId, GeneroId) VALUES (@LivroId, @GeneroId)";
+        string cs = @"Data Source=.\SQLEXPRESS;Initial Catalog=CJ3027481PR2;Integrated Security=True";
+     
         string caminhoFoto = "";
         private void Btnimage_Click(object sender, EventArgs e)
         {
@@ -28,9 +32,130 @@ namespace Livraria
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 caminhoFoto = ofd.FileName;
-                pictureBox2.ImageLocation = caminhoFoto; // troque pelo nome do seu PictureBox
+                Picturebook.ImageLocation = caminhoFoto; // troque pelo nome do seu PictureBox
             }
 
         }
+        
+        private void Btnadd_Click(object sender, EventArgs e)
+        {
+           
+
+            var autores = TxtAutor.Text.Split(','); // divide pelos autores digitados
+            byte[] fotoBytes = null;
+
+            if (Picturebook.Image != null)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    Picturebook.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    fotoBytes = ms.ToArray();
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlTransaction tx = con.BeginTransaction();
+
+
+                try
+                {
+                    // 1) Inserir o livro e pegar o ID gerado
+                    string sqlLivro = @"INSERT INTO Livros (Nome, Editora, Preco, Foto)
+                                OUTPUT INSERTED.Id
+                                VALUES (@Nome, @Editora, @Preco, @Foto)";
+                    int livroId;
+                    using (SqlCommand cmd = new SqlCommand(sqlLivro, con, tx))
+                    {
+                        cmd.Parameters.AddWithValue("@Nome", Txtnamebook.Text);
+                        cmd.Parameters.AddWithValue("@Editora", TxtEditora.Text);
+                        cmd.Parameters.AddWithValue("@Preco", decimal.Parse(TxtPreco.Text));
+                        cmd.Parameters.AddWithValue("@Foto", (object)fotoBytes ?? DBNull.Value);
+                        livroId = (int)cmd.ExecuteScalar();
+                    }
+
+                    // 2) Para cada autor, garantir que ele existe em Autores
+                    foreach (var autorNome in autores)
+                    {
+                        string nomeAutor = autorNome.Trim();
+                        if (string.IsNullOrEmpty(nomeAutor)) continue;
+
+                        // Tenta pegar Id existente
+                        int autorId;
+                        string sqlSelect = "SELECT Id FROM Autores WHERE Nome = @Nome";
+                        using (SqlCommand cmdSel = new SqlCommand(sqlSelect, con, tx))
+                        {
+                            cmdSel.Parameters.AddWithValue("@Nome", nomeAutor);
+                            object result = cmdSel.ExecuteScalar();
+                            if (result != null)
+                            {
+                                autorId = (int)result;
+                            }
+                            else
+                            {
+                                // Se não existe, insere e pega o novo Id
+                                string sqlAutor = @"INSERT INTO Autores (Nome)
+                                            OUTPUT INSERTED.Id
+                                            VALUES (@Nome)";
+                                using (SqlCommand cmdAutor = new SqlCommand(sqlAutor, con, tx))
+                                {
+                                    cmdAutor.Parameters.AddWithValue("@Nome", nomeAutor);
+                                    autorId = (int)cmdAutor.ExecuteScalar();
+                                }
+                            }
+                        }
+                        string[] generos = TxtGenero.Text.Split(',')
+                           .Select(g => g.Trim())
+                           .Where(g => g.Length > 0)
+                           .ToArray();
+                        foreach (string nomeGenero in generos)
+                        {
+                            // 1) Pega Id do gênero (se existir)
+                            int generoId;
+                            string sqlSelect = "SELECT Id FROM Generos WHERE Nome = @Nome";
+                            using (SqlCommand cmdSel = new SqlCommand(sqlSelect, con, tx))
+                            {
+                                cmdSel.Parameters.AddWithValue("@Nome", nomeGenero);
+                                object result = cmdSel.ExecuteScalar();
+                                if (result != null)
+                                {
+                                    generoId = Convert.ToInt32(result);
+                                }
+                                else
+                                {
+                                    // 2) Insere gênero e pega o Id
+                                    string sqlInserirGenero = "INSERT INTO Generos (Nome) OUTPUT INSERTED.Id VALUES (@Nome)";
+                                    using (SqlCommand cmdIns = new SqlCommand(sqlInserirGenero, con, tx))
+                                    {
+                                        cmdIns.Parameters.AddWithValue("@Nome", nomeGenero);
+                                        generoId = (int)cmdIns.ExecuteScalar();
+                                    }
+                                }
+                            }
+
+                            // 3) Insere relação (evita duplicata)
+                            string sqlRel = @"IF NOT EXISTS (SELECT 1 FROM LivroGeneros WHERE LivroId=@LivroId AND GeneroId=@GeneroId)
+                            INSERT INTO LivroGeneros (LivroId, GeneroId) VALUES (@LivroId, @GeneroId)";
+                            using (SqlCommand cmdRel = new SqlCommand(sqlRel, con, tx))
+                            {
+                                cmdRel.Parameters.AddWithValue("@LivroId", livroId);
+                                cmdRel.Parameters.AddWithValue("@GeneroId", generoId);
+                                cmdRel.ExecuteNonQuery();
+                            }
+                        }
+                    }
+
+                    tx.Commit();
+                    MessageBox.Show("Livro e autores cadastrados com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    tx.Rollback();
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+        }
     }
-}
+    }
+
